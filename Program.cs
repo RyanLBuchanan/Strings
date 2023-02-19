465542ff00c42e02da7f32ff500e401e618aa27d
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-19 10:59:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region EmptyOrNull 
+            String nullString = null;
+            String emptyString = "";
+
+            Console.WriteLine($"nullString is null or empty: {String.IsNullOrEmpty(nullString)}");
+            Console.WriteLine($"emptyString is null or empty: {String.IsNullOrEmpty(emptyString)}");
+            #endregion
+        }
+    }
+}
